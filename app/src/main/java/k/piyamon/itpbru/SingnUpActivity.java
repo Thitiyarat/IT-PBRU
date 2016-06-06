fddf28f8a699dfe4719ce92b8d501cdd435a26bb
piyamon <girlpiyamon@gmail.com>
2016-06-06 02:46:45 -0700

    ออกแบบหน้า singn Up

---
+++ app/src/main/java/k/piyamon/itpbru/SingnUpActivity.java
@@ -0,0 +1,13 @@
+package k.piyamon.itpbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingnUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singn_up);
+    } // Main Method
+} //Main class
